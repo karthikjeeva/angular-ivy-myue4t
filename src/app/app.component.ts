670cf5cdf1ed80a253cc1a7da5774fc71933d3f6
karthikjeeva <karthikjeeva2206@gmail.com>
2021-07-10 18:06:19 +0200

    7 files modified, 11 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,8 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-
-export interface Person {
-  name: string;
-  age: Number;
-}
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  names: Person[] = [
-    { name: 'kar', age: 22 },
-    { name: 'poo', age: 21 },
-    { name: 'smi', age: 16 }
-  ];
-  myControl: FormControl = new FormControl();
-  filteredOptions: Observable<Person[]> | undefined;
-
-  ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => (typeof value === 'string' ? value : value.name)),
-      map(name => (name ? this._filter(name) : this.names.slice()))
-    );
-  }
-
-  displayFn(user: Person): string {
-    return user && user.name ? user.name : '';
-  }
-
-  private _filter(name: string): Person[] {
-    const filterValue = name.toLowerCase();
-
-    return this.names.filter(option =>
-      option.name.toLowerCase().includes(filterValue)
-    );
-  }
-}
+export class AppComponent {}
---
+++ src/app/matautoobject/matautoobject.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-matautocom',
+  templateUrl: './matautocom.component.html',
+  styleUrls: ['./matautocom.component.css']
+  //encapsulation: ViewEncapsulation.None,
+})
+export class MatautoobjectComponent implements OnInit {
+  names: string[] = ['ghui', 'ustu', 'caty', 'momo', 'rekh', 'john', 'kemp'];
+  myControl: FormControl = new FormControl();
+  filteredOptions: Observable<string[]> | undefined;
+  @Input() disableRipple = true;
+
+  constructor() {}
+  //api
+  @Input() autoActiveFirstOption = true;
+  @Input('class') classList = ['boldOptions', 'coloroptions'];
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value: string) => this._filter(value))
+    );
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.names.filter(option =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
+}
